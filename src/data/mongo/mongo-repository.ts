4715afdc5f5e60343ddba46fa2f6f50f8db36129
Gabriel Borges <gabrielfr.borges@gmail.com>
2023-05-13 17:23:10 -0300

    chore: delete unnecessary code

--- src/data/mongo/mongo-repository.ts
+++ src/data/mongo/mongo-repository.ts
@@ -1,17 +1,14 @@
-import { Entity } from "@/domain/commons/types";
 import { WithId } from "mongodb";
-import { ObjectId } from "mongodb";
 import { Collection } from "mongodb";
 
 export class MongoRepository {
-    protected static readonly COLLECTION: string;
     constructor(protected readonly collection: Collection) {}
 
     protected map<T>(document: WithId<any>): T {
         const id  = document._id.toHexString();
         delete document._id;
         document.id = id;
-        
+
         return document;
     }
 
