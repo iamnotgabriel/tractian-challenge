1c1c1c0cbb0f40e2d5fd29060cbe7d2516595f0b
Gabriel Borges <gabrielfr.borges@gmail.com>
2023-05-16 13:01:10 -0300

    chore: manualy fomat code middlewares

--- src/api/http/http-reponse.ts
+++ src/api/http/http-reponse.ts
@@ -1,6 +1,5 @@
 import { StatusCode } from "./status-code"
-import { ApplicationError, ErrorCodes } from "@/domain/errors"
-
+import { ErrorCodes } from "@/domain/errors"
 
 export type HttpResponse<T= any> = {
     status: StatusCode | ErrorCodes,
